54e63f9835109e38a5343b090d0ced57012569d5
Dylan Ang <dylanmouang@gmail.com>
2022-01-11 16:44:05 -0800

    Add CAN Message class and string conversion

---
+++ testing/index.py
@@ -0,0 +1,28 @@
+import datetime as dt
+
+
+class Message:
+    def __init__(self, id: int, payload: list) -> None:
+        self.id = id
+        # create list of 8 bytes to store data in
+        self.buf = list(word for word in payload)
+        self.timestamp = dt.datetime.now()
+
+    def str_form(self) -> str:
+        m = ""
+        m += str(hex(self.id)) + ','
+        for word in self.buf:
+            m += str(word) + ','
+        m += self.timestamp.strftime("%H:%M:%S")
+        return m
+
+
+
+# def index(id: int):
+    '''
+
+    '''
+
+if __name__ == '__main__':
+    m = Message(17, [1, 2, 3, 4, 5, 6, 7, 8])
+    print(m.str_form())
